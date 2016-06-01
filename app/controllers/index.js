939267d3333ae04da6a484a290bc4ec755e3c18b
liyueyang <liyueyang1991@163.com>
2016-06-01 09:51:25 +0800

    category_not_finish

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,23 @@
 var Song = require('../models/song');
+var Category = require('../models/category');
+
 //负责与首页进行交互
 
 exports.index = function(req, res){
 	//index page
-	console.log(req.session.user); //可以获取session中的登录状态
-	
-	Song.fetch(function(err, songs){
-		if(err){
-			console.log(err);
-		}
-		
-		res.render('index', {
-			title: 'imusic 首页',
-			songs: songs
+	Category
+		.find({})
+		.populate({path: 'songs', options: {limit: 5}})
+		.exec(function(err, categories){
+			if(err){
+				console.log(err);
+			}
+
+			res.render('index', {
+				title: 'imusic 首页',
+				categories: categories
+			});
 		});
-	});
 };
 
 
